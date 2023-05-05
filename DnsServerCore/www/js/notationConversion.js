4ff484a716ae99fb5a74adbdbab64decb8ccb6da
Rustu Koprulu <rustulp@gmail.com>
2023-05-05 13:39:30 -0600

    Added notation conversion functions

---
+++ DnsServerCore/www/js/notationConversion.js
@@ -0,0 +1,30 @@
+﻿/* RK - ADDED MAY 4 2023 */
+
+function convertDidNotationToDnsNotation(input) {
+
+    let output = "";
+    if (input.substring(0, 4) === "did:") {
+        let didLabels = input.split(":");
+
+        for (let i = didLabels.length - 1; i >= 0; i--) {
+            output += didLabels[i];
+            if (i > 0) output += ".";
+        }
+    }
+    return output;
+
+}
+
+function convertDnsNotationToDidNotation(input) {
+
+    let output = "";
+    if (input.substring(0, 4) !== "did:") {
+        let dnsLabels = input.split(".");
+
+        for (let i = dnsLabels.length - 1; i >= 0; i--) {
+            output += dnsLabels[i];
+            if (i > 0) output += ":";
+        }
+    }
+    return output;
+}
