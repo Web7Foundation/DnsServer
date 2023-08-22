b01350ec226463d6fdaf1d3af042d31cff0f4551
Russ Koprulu <rustulp@gmail.com>
2023-08-22 05:20:01 -0600

    added file

---
+++ DnsServerCore/www/js/didcomm-json-helper.js
@@ -0,0 +1,25 @@
+﻿function toPrettyJson(input) {
+    return JSON.stringify(JSON.parse(input), null, 2);
+}
+
+function isValidJson(str) {
+    try {
+        return JSON.parse(str) && !!str;
+    } catch (e) {
+        return false;
+    }
+}
+
+function parseVmmKeys() {
+
+}
+
+
+// Copy to clipboard button event handler
+$(document).on("click", "#VMM_formattedDataCopyBtn", function () {
+    // TODO
+    // https://www.freecodecamp.org/news/copy-text-to-clipboard-javascript/
+});
+
+
+
